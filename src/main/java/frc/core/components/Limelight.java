92345580093e928269cf593dcf15a6a10732007c
unknown <biancadsalves@gmail.com>
2021-02-07 21:36:13 -0300

    limelight component

---
+++ src/main/java/frc/core/components/Limelight.java
@@ -0,0 +1,91 @@
+package frc.core.components;
+
+import edu.wpi.first.networktables.NetworkTableEntry;
+import edu.wpi.first.networktables.NetworkTableInstance;
+
+public class Limelight {
+    private static final String TABLE_NAME = "limelight";
+
+    public enum Entry {
+        TX("tx"),
+        TY("ty"),
+        TA("ta"),
+        TV("tv")
+        PIPELINE("pipeline"),
+        LED_MODE("ledMode"),
+        CAM_MODE("camMode");
+
+        public final String key;
+        private Entry(String key) {
+            this.key = key;
+        }
+    }
+
+    public enum LedMode {
+        CURRENT(0),
+        OFF(1),
+        BLINK(2),
+        ON(3);
+
+        public final int value;
+        private LedMode(int value) {
+            this.value = value;
+        }
+    }
+
+    public enum CamMode {
+        VISION_PROCESSOR(0),
+        DRIVER_CAMERA(1);
+
+        public final int value;
+        private CamMode(int value) {
+            this.value = value;
+        }
+    }
+
+    private static NetworkTableEntry getEntry(Entry entry) {
+        var table = NetworkTableInstance.getDefault().getTable(TABLE_NAME);
+
+        return table.getEntry(entry.key);
+    }
+
+    private static double getValue(Entry entry) {
+        return getEntry(entry).getDouble(0.0);
+    }
+
+    public static double getX() {
+        return getValue(Entry.TX);
+    }
+
+    public static double getY() {
+        return getValue(Entry.TY);
+    }
+
+    public static double getA() {
+        return getValue(Entry.TA);
+    }
+
+    public static double getV() {
+        return getValue(Entry.TV);
+    }
+
+    public static boolean isTarget() {
+        return getV() == 1;
+    }
+
+    private static void setEntry(Entry entry, int value) {
+        getEntry(entry).setNumber(value);
+    }
+
+    public static void setPipeline(int value) {
+        setEntry(Entry.PIPELINE, value);
+    }
+
+    public static void setLed(LedMode ledMode) {
+        setEntry(Entry.LED_MODE, ledMode.value);
+    }
+
+    public static void setCam(CamMode camMode) {
+        setEntry(Entry.CAM_MODE, camMode.value);
+    }
+}
