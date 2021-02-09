c11b9f617d7bc8987a579c64e62656b1dea37b60
Gabriela Tomaz <gabrielatomaz1772@gmail.com>
2021-02-09 20:10:56 -0300

    fix indentation

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -3,56 +3,56 @@ package frc.robot;
 import edu.wpi.first.wpilibj.kinematics.DifferentialDriveKinematics;
 
 public final class Constants {
-    public static final class DrivetrainConstants {
-        //motors
-        public static final int[]
-        motorRightPort = new int[] { 0, 1 },
-        motorLeftPort = new int[] { 2, 3 };
+	public static final class DrivetrainConstants {
+		//motors
+		public static final int[]
+		motorRightPort = new int[] { 0, 1 },
+		motorLeftPort = new int[] { 2, 3 };
 
-        //encoders
-        public static final int[]
-        encoderRightPort = new int[] { 6, 7 },
-        encoderLeftPort = new int[] { 8, 9 };
+		//encoders
+		public static final int[]
+		encoderRightPort = new int[] { 6, 7 },
+		encoderLeftPort = new int[] { 8, 9 };
 
-        public static final boolean
-        isEncoderRightInverted = true;
+		public static final boolean
+		isEncoderRightInverted = true;
 
-        public static final int 
-        pulsesRight = 498,
-        pulsesLeft = 494;
+		public static final int 
+		pulsesRight = 498,
+		pulsesLeft = 494;
 
-        //chassi
-        public static final int 
-        wheelRadius = 2;
+		//chassi
+		public static final int 
+		wheelRadius = 2;
 
-        //voltageConstraint
-        public static final double 
-        ksVolts = 0.716, //kS
-        kvVoltSecondsPerMeter = 2.23, //kV
-        kaVoltSecondsSquaredPerMeter = 0.189, //kA
-        kTrackwidthMeters = 2.84,
-        differentialDriveVoltageConstraintMaxVoltage = 10;
+		//voltageConstraint
+		public static final double 
+		ksVolts = 0.716, //kS
+		kvVoltSecondsPerMeter = 2.23, //kV
+		kaVoltSecondsSquaredPerMeter = 0.189, //kA
+		kTrackwidthMeters = 2.84,
+		differentialDriveVoltageConstraintMaxVoltage = 10;
 
-        public static final DifferentialDriveKinematics
-        kDriveKinematics = new DifferentialDriveKinematics(kTrackwidthMeters);
+		public static final DifferentialDriveKinematics
+		kDriveKinematics = new DifferentialDriveKinematics(kTrackwidthMeters);
 
-        //PID
-        public static final double 
-        kPDriveVelocity = 2.21,
-        kIDriveVelocity = 0,
-        kDDriveVelocity = 0;   
-    }
+		//PID
+		public static final double 
+		kPDriveVelocity = 2.21,
+		kIDriveVelocity = 0,
+		kDDriveVelocity = 0;   
+	}
 
-    public static final class OIConstants {
-        public static final int
-        driverControllerPort = 0;
-    }
+	public static final class OIConstants {
+		public static final int
+		driverControllerPort = 0;
+	}
 
-    public static final class AutoConstants {
-        public static final double 
-        kMaxSpeedMetersPerSecond = 2,
-        kMaxAccelerationMetersPerSecondSquared = 2,
-        kRamseteB = 2,
-        kRamseteZeta = 0.7;
-    }
+	public static final class AutoConstants {
+		public static final double 
+		kMaxSpeedMetersPerSecond = 2,
+		kMaxAccelerationMetersPerSecondSquared = 2,
+		kRamseteB = 2,
+		kRamseteZeta = 0.7;
+	}
 }
