ca5e91419cbcf6ace7910e13627a0950742000e1
Guilherme Luvielmo <luvielmo.guilherme@gmail.com>
2021-03-03 09:56:32 -0300

    fix double button

--- src/main/java/frc/core/util/DoubleButton.java
+++ src/main/java/frc/core/util/DoubleButton.java
@@ -1,15 +1,14 @@
 package frc.core.util;
 
-import edu.wpi.first.wpilibj.XboxController;
-// import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
 
 public class DoubleButton extends Trigger {
 
-    private final XboxController joystick;
+    private final GenericHID joystick;
     private final int buttonOne, buttonTwo;
 
-    public DoubleButton(XboxController joystick, int buttonOne, int buttonTwo) {
+    public DoubleButton(GenericHID joystick, int buttonOne, int buttonTwo) {
         this.joystick = joystick;
         this.buttonOne = buttonOne;
         this.buttonTwo = buttonTwo;
