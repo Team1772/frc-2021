92feaa141a2ce31efb691d4c000530a81bae18d5
BrunoOsio <osiobruno716@gmail.com>
2021-03-13 17:33:19 -0300

    created classes for each auto

---
+++ src/main/java/frc/robot/commands/autons/Bounce.java
@@ -0,0 +1,16 @@
+package frc.robot.commands.autons;
+
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.core.util.TrajectoryBuilder;
+
+// speed = 1.5
+// aceleration = 1
+
+public class Bounce extends SequentialCommandGroup {
+
+  public Bounce(TrajectoryBuilder trajectoryBuilder) {
+    super.addCommands(
+      trajectoryBuilder.buildTrajectory("bounce")
+    );
+  }
+}
--- src/main/java/frc/robot/commands/autons/GalacticB.java
+++ src/main/java/frc/robot/commands/autons/GalacticB.java
@@ -3,11 +3,11 @@ package frc.robot.commands.autons;
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import frc.core.util.TrajectoryBuilder;
 
-public class GalacticA extends ParallelCommandGroup {
+public class GalacticB extends ParallelCommandGroup {
 
-  public GalacticA(TrajectoryBuilder trajectoryBuilder) {
+  public GalacticB(TrajectoryBuilder trajectoryBuilder) {
     super.addCommands(
-      trajectoryBuilder.buildTrajectory("galacticA")
+      trajectoryBuilder.buildTrajectory("galacticB")
     );
   }
 }
