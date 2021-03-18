e8789cf2448ea775bcf25977b6bd19f58d1e2659
BrunoOsio <osiobruno716@gmail.com>
2021-03-18 11:52:17 -0300

    constants for colors

--- src/main/java/frc/robot/commands/autons/GalacticA.java
+++ src/main/java/frc/robot/commands/autons/GalacticA.java
@@ -3,6 +3,7 @@ package frc.robot.commands.autons;
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import frc.core.components.Limelight;
 import frc.core.util.TrajectoryBuilder;
+import frc.robot.Constants.FieldConstants;
 import frc.robot.commands.buffer.SmartFeed;
 import frc.robot.commands.intake.CollectPowerCell;
 import frc.robot.subsystems.Buffer;
@@ -12,8 +13,8 @@ public class GalacticA extends ParallelCommandGroup {
   public GalacticA(TrajectoryBuilder trajectoryBuilder, Buffer buffer, Intake intake) {
     Limelight.setPipeline(1);
 
-    var galacticCommand = Limelight.isTarget() ? "red" : "blue";
-    var path =  String.format("galacticA_%s", galacticCommand);
+    var allianceColor = Limelight.isTarget() ? FieldConstants.red : FieldConstants.blue;
+    var path =  String.format("galacticA_%s", allianceColor);
 
     super.addCommands(
       trajectoryBuilder.build(path),
