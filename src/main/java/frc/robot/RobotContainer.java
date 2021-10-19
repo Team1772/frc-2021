0e0526cb80060967425707646bcc4a770c84d1de
Bruno <osiobruno716@gmail.com>
2021-10-19 15:00:57 -0300

    deleted shooter in robot container

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -6,22 +6,16 @@ import edu.wpi.first.wpilibj.XboxController.Button;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
-import edu.wpi.first.wpilibj2.command.button.Trigger;
 import frc.robot.subsystems.Intake;
-import frc.robot.subsystems.Shooter;
 import frc.core.util.TrajectoryBuilder;
-import frc.core.util.DoubleButton;
 import frc.robot.Constants.OIConstants;
 import frc.robot.commands.intake.CollectPowerCell;
 import frc.robot.commands.intake.ReleasePowerCell;
-import frc.robot.commands.shooter.ShootPowerCellAngle;
-import frc.robot.commands.shooter.ShootPowerCellDefault;
 import frc.robot.commands.autons.GalacticA;
 import frc.robot.commands.drivetrain.AimTarget;
 import frc.robot.commands.drivetrain.ArcadeDrive;
 import frc.robot.commands.drivetrain.CurvatureDrive;
 import frc.robot.commands.buffer.SmartFeed;
-import frc.robot.commands.buffer.Feed;
 import frc.robot.subsystems.Drivetrain;
 import frc.robot.subsystems.Buffer;
 
@@ -30,7 +24,6 @@ public class RobotContainer {
   private final Drivetrain drivetrain;
   private final Intake intake;
   private final Buffer buffer;
-  private final Shooter shooter;
 
   private final XboxController driver, operator;
   
@@ -40,19 +33,13 @@ public class RobotContainer {
     this.drivetrain = new Drivetrain();
     this.intake = new Intake();
     this.buffer = new Buffer();
-    this.shooter = new Shooter();
 
     this.driver = new XboxController(OIConstants.driverControllerPort);
     this.operator = new XboxController(OIConstants.operatorControllerPort);
 
     this.trajectoryBuilder = new TrajectoryBuilder(
       this.drivetrain, 
-      "autoAwards"
-      // "barrel",
-      // "slalom", 
-      // "galacticA",
-      // "galacticB",
-      // "bounce"
+      "test"
     );
 
     this.configureButtonBindings();
@@ -61,7 +48,6 @@ public class RobotContainer {
   private void configureButtonBindings() {
     this.configureButtonBindingsIntake();
     this.configureButtonBindingsBuffer();
-    this.configureButtonBindingsShooter();
     this.configureButtonBindingsDrivetrain();
   }
 
@@ -80,24 +66,6 @@ public class RobotContainer {
     this.buffer.setDefaultCommand(
       new SmartFeed(this.buffer)
     );
-  }
-
-   private void configureButtonBindingsShooter() {
-    var buttonBumperRight = new JoystickButton(this.operator, Button.kBumperRight.value);
-    
-    Trigger isAtSettedVelocity = new Trigger(() -> this.shooter.atSettedVelocity());
-
-    buttonBumperRight
-      .whileHeld(new ShootPowerCellDefault(this.shooter))
-      .and(isAtSettedVelocity).whileActiveContinuous(new Feed(this.buffer));
-    
-    var doubleButton = new DoubleButton(
-      this.operator,
-      Button.kBumperRight.value,
-      Button.kA.value
-    );
-
-    doubleButton.whileActiveContinuous(new ShootPowerCellAngle(this.shooter));
   }
 
   public void configureButtonBindingsDrivetrain() {
